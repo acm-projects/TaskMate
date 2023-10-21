a393d37a957bc10ed3b52970f7aede8bf502b379
Devansh <devagr3924@gmail.com>
2023-10-20 21:19:19 -0500

    Merge issue fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
+import 'pages/intro_page.dart';
 import 'pages/signin_page.dart';
-<<<<<<< HEAD
-import 'pages/signup_page1.dart';
-=======
 import 'pages/home.dart';
->>>>>>> 4a9b7bf7cc97b4c1ea633c404e970541d6fed861
 
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -18,14 +16,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: const Home(),
       routes: {
-        '/signin_page.dart': (context)=> SignInPage(),
-<<<<<<< HEAD
-        '/signup_page1.dart': (context) => const SignUpPage1(),
+        '/signin_page.dart': (context) => SignInPage(),
+        '/intro_page.dart': (context) => IntroPage(),
       },
-=======
-        '/intro_page.dart': (context)=> IntroPage(),
-        },
->>>>>>> 4a9b7bf7cc97b4c1ea633c404e970541d6fed861
     );
   }
 }
