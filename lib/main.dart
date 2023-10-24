9b0c09cc76f032285eaf045b2e2633df5798d8df
zubsyeda <125157533+zubsyeda@users.noreply.github.com>
2023-10-23 23:54:22 -0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_mate/pages/intro_page.dart';
 import 'pages/signin_page.dart';
 import 'pages/signup_page1.dart';
 import 'pages/home.dart';
@@ -13,8 +14,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const Home(),
+      home: const IntroPage(),
       routes: {
+        '/home.dart': (context)=> Home(),
         '/signin_page.dart': (context)=> SignInPage(),
         '/signup_page1.dart': (context) => SignUpPage1(),
       },
