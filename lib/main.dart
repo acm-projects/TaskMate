fd41021cfc651b1ede6ba4a28100c6c07217c657
UbaidM0 <usmohammed0@gmail.com>
2023-10-29 01:33:33 -0500

    tasks page done

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FriendsPage(),
+      home: TasksPage(),
       routes: {
         '/intro_page.dart': (context) => IntroPage(),
         '/signin_page.dart': (context) => SignInPage(),
