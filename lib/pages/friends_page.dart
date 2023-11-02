edaeaf5e75e2e873c2a268beb53a613b2419c884
zubsyeda <125157533+zubsyeda@users.noreply.github.com>
2023-11-01 20:45:21 -0500

    friends

--- lib/pages/friends_page.dart
+++ lib/pages/friends_page.dart
@@ -9,46 +9,107 @@ class FriendsPage extends StatelessWidget {
       body:DecoratedBox(
         decoration: const BoxDecoration(
           image: DecorationImage(
-                image: AssetImage("lib/images/GroupCorner.png"), 
+                image: AssetImage("lib/images/whitebg+gradient.png"), 
                 fit: BoxFit.cover
           ),
          ),
-        child: Center(
-          child: ListView(
-            children:[
-              Container(
-                width: 336,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+         child: SafeArea(
+          child: Center(
+            child: Stack(
+              children: [
+                ListView(
+                  padding: EdgeInsets.only(right: 25, left: 25),
                   children: [
                     Row(
                       children: [
-                        Container(
-                          height: 32,
-                          child: Image.asset('lib/images/profile.png')
+                      IconButton(
+                          padding: EdgeInsets.all(0),
+                          icon: const CircleAvatar(
+                            backgroundImage:
+                                AssetImage('lib/images/profile.png'),
+                            backgroundColor: Colors.grey,
                           ),
-                        Text('Friend1'),
+                          iconSize: 45,
+                          onPressed: () {
+                            //add functionality
+                          }),
+                    ]),
+                    const SizedBox(height: 5),
+                    const Row(
+                      children: [
+                        Text(
+                          'Friends',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontFamily: 'Jockey One',
+                            fontWeight: FontWeight.w400,
+                            height: 0,
+                          ),
+                        ),
                       ],
                     ),
-
                     Container(
-                      width: 128,
-                      padding: EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF7E80FF),
-                        borderRadius: BorderRadius.circular(10),
+                      height: 1.5,
+                      color: Color(0xFFBFBFFF),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 8, bottom: 8),
+                      child: Text(
+                        'Make a New Friend Today!',
+                        style: TextStyle(
+                          color: Color(0xFF808080),
+                          fontSize: 16,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w400,
+                          height: 0,
+                        ),
                       ),
-                      child: const Text('Unfollow'),
-                      
-                    )
+                    ),
+                    
                   ],
-                ),
-        
-              )
-              
-            ],
+                )
+              ],
+            ),
+          )
           ),
-        ),
+        // child: Center(
+        //   child: ListView(
+        //     children:[
+        //       Container(
+        //         width: 336,
+        //         child: Row(
+        //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //           children: [
+        //             Row(
+        //               children: [
+        //                 Container(
+        //                   height: 32,
+        //                   child: Image.asset('lib/images/profile.png')
+        //                   ),
+        //                 Text('Friend1'),
+        //               ],
+        //             ),
+
+        //             Container(
+        //               width: 128,
+        //               padding: EdgeInsets.all(5),
+        //               decoration: BoxDecoration(
+        //                 color: Color(0xFF7E80FF),
+        //                 borderRadius: BorderRadius.circular(10),
+        //               ),
+        //               child: const Text('Unfollow'),
+                      
+        //             )
+        //           ],
+        //         ),
+        
+        //       )
+              
+        //     ],
+        //   ),
+        // ),
       )
     );
   }
